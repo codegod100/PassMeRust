6deab04fe718c15912724303bc8baa328268d02c
Veronika Gillies <veronikamwinters@gmail.com>
2023-09-09 04:45:45 +0000

    Add weak password check to password test
    Previously, the password test only checked for "hello universe" and "password". This commit adds a check for the "yolo" password, categorizing it as "Weak".

--- passme.test.js
+++ passme.test.js
@@ -2,5 +2,6 @@ import {test,expect} from "bun:test"
 import {check_password} from "./passme.js"
 test("test password",()=>{
     expect(check_password("hello universe")).toBe("Password strength: Very Strong")
+    expect(check_password("yolo")).toBe("Password strength: Weak")
     expect(check_password("password")).toBe("Password is weak: It matches a common password or dictionary word.")
 })
