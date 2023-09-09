f522aa1af8fa370c4901b0caecd549931f428d85
Veronika Gillies <veronikamwinters@gmail.com>
2023-09-09 04:40:37 +0000

    Add test cases for password strength checks: a very strong password and a weak password that matches a common password or dictionary word.

--- passme.test.js
+++ passme.test.js
@@ -2,4 +2,5 @@ import {test,expect} from "bun:test"
 import {check_password} from "./passme.js"
 test("test password",()=>{
     expect(check_password("hello universe")).toBe("Password strength: Very Strong")
+    expect(check_password("password")).toBe("Password is weak: It matches a common password or dictionary word.")
 })
